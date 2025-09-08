8a95075a384bcfff502dfb3d458fa8a9ed8002df
Valery <v.davydkin@expero.ru>
2025-09-08 09:56:24 +0700

    dev WIP

--- index.php
+++ index.php
@@ -1,3 +1,22 @@
 <?php
 
+use HomeCalculator\App;
+
 require_once "vendor/autoload.php";
+
+$app = App::init();
+?>
+
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title><?= $app->getName() ?></title>
+</head>
+<body>
+    <h1><?= $app->getName() ?></h1>
+</body>
+</html>
--- src/App.php
+++ src/App.php
@@ -4,7 +4,24 @@ declare(strict_types=1);
 
 namespace HomeCalculator;
 
-class App
+final class App
 {
+    private string $name;
 
+    private static self $instance;
+
+    private function __construct()
+    {
+        $this->name = "Home calculator";
+    }
+
+    public static function init(): self
+    {
+        return self::$instance ??= new self();
+    }
+
+    public function getName(): string
+    {
+        return $this->name;
+    }
 }
